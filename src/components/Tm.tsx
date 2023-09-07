1e11079f0c71bc3c68fef716aefd30d9e2263ef3
Parthib DS <parthib22.ds@gmail.com>
2023-09-07 16:47:04 +0530

    map by pds

--- src/components/Tm.tsx
+++ src/components/Tm.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Tm = () => {
     return (
         <div>
-
+            <h1>NAME</h1>
+            <p>I am a student at the University of Toronto. I'm majoring in Computer Science and minoring in Mathematics.</p>
         </div>
     )
 }
