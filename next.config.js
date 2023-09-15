c170fc0107618ac34c57a032a93b1a3fbd036d88
THUNDERSAMA <77087243+THUNDERSAMA@users.noreply.github.com>
2023-09-15 15:07:52 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const withPWA=require ('next-pwa');
+// const nextConfig = {
+//     pwa:{
+//         dest:"public",
+//         register:true,
+//         skipWaiting:true,
+//         disable:process.env.NODE_ENV === 'development'
+//     }
+// }
 
-module.exports = nextConfig
+// module.exports = withPWA(nextConfig)
+const withPWA = require('next-pwa')({
+    dest:"public",
+    register:true,
+    skipWaiting:true,
+    disable:process.env.NODE_ENV === 'development'
+      })
+    
+    /** @type {import('next').NextConfig} */
+    const nextConfig = {
+    reactStrictMode: true,
+     }
+    
+    module.exports = withPWA(nextConfig);
