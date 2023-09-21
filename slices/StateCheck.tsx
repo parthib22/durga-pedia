52e075e4a19e48d2a1d9dde5275c9767bd3634e3
THUNDERSAMA <77087243+THUNDERSAMA@users.noreply.github.com>
2023-09-21 20:43:29 +0530

    changes

---
+++ slices/StateCheck.tsx
@@ -0,0 +1,33 @@
+// NewGlobalStore.tsx
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../src/app/store'
+
+// Define a type for the new slice state
+export interface StateCheck {
+  // Define properties for your new global variable here
+  someProperty: any,
+}
+
+// Define the initial state for the new slice
+const initialState: StateCheck = {
+  someProperty: '', // Initialize properties as needed
+}
+
+export const NewGlobalStore = createSlice({
+  name: 'statecheck', // Unique name for the new slice
+  initialState,
+  reducers: {
+    // Define actions and reducers for the new global variable here
+    setSomeProperty: (state, action: PayloadAction<any>) => {
+      state.someProperty = action.payload
+    },
+    // Add more reducers as needed
+  },
+})
+
+export const { setSomeProperty } = NewGlobalStore.actions
+
+// Selector for accessing the new global variable
+export const selectSomeProperty = (state: RootState) => state.statecheck.someProperty
+
+export default NewGlobalStore.reducer
