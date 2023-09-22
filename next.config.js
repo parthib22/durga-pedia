0409e5519b4473bdf8fc933e1ad685fbaa1c0d8f
Biswaraj21 <111447635+Biswaraj21@users.noreply.github.com>
2023-09-22 19:32:40 +0530

    changes by biswa

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const withPWA = require('next-pwa')({
     /** @type {import('next').NextConfig} */
     const nextConfig = {
     reactStrictMode: true,
-        output: 'export'
+        
      }
     
     module.exports = withPWA(nextConfig);
