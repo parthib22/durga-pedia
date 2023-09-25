7d86033ee6d22bb188d9a1b33de8b9da3fb9ecb6
THUNDERSAMA <77087243+THUNDERSAMA@users.noreply.github.com>
2023-09-25 12:19:17 +0530

    changes

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -111,7 +111,7 @@ const AutoComplete = () => {
         {/* <span className="shareIco">{<IosShareIcon />}</span> */}
         <input
           // className={searchTerm.length > 0 ? "ipPadding" : ""}
-          placeholder="Happy Birthday Parthib!"
+          placeholder="Search pandal"
           type="text"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
