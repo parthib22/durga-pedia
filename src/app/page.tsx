f941c73d5fe63c279224d2bea4ae5720ba3446b0
THUNDERSAMA <77087243+THUNDERSAMA@users.noreply.github.com>
2023-10-08 10:12:31 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,15 @@ import Cards from "@/components/Cards";
 import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
 import CloseIcon from "@mui/icons-material/Close";
 import Image from "next/image";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import ImageIcon from "@mui/icons-material/Image";
+import WorkIcon from "@mui/icons-material/Work";
+import BeachAccessIcon from "@mui/icons-material/BeachAccess";
+import { Typography } from "@mui/material";
 
 export default function Home() {
   const [passCords, setCords] = useState(""); //changes by sam
@@ -33,6 +42,57 @@ export default function Home() {
                 <CloseIcon />
               </button>
             </h3>
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Typography
+                    variant="h6"
+                    style={{ color: "#ff4500", fontWeight: "bold" }}
+                  >
+                    Step 1
+                  </Typography>
+                }
+                secondary={
+                  <Typography style={{ color: "black", fontWeight: "700" }}>
+                    Enter your start location
+                  </Typography>
+                }
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Typography
+                    variant="h6"
+                    style={{ color: "#ff4500", fontWeight: "bold" }}
+                  >
+                    Step 2
+                  </Typography>
+                }
+                secondary={
+                  <Typography style={{ color: "black", fontWeight: "700" }}>
+                    Choose no of pandals/range
+                  </Typography>
+                }
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Typography
+                    variant="h6"
+                    style={{ color: "#ff4500", fontWeight: "bold" }}
+                  >
+                    Step 3
+                  </Typography>
+                }
+                secondary={
+                  <Typography style={{ color: "black", fontWeight: "700" }}>
+                    Press route to get your ideal pandal hopping plan
+                  </Typography>
+                }
+              />
+            </ListItem>
             <div className="tSteps">
               <img className="tutimg" src="/tutorial.gif" alt="pub" />
             </div>
